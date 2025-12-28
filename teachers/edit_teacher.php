deef101b2904670a4bc9716d5b5f5e5cff741558
MuhammadFarouk12 <muhammad12fk@gmail.com>
2025-12-28 09:04:45 +0300

    Add the Teachers Pages

---
+++ teachers/edit_teacher.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Edit Teacher </title>
+	<?php include('../utilities/utilities.php'); ?>
+</head>
+<body>
+<?php
+
+	if (!isset($_GET['id'])) die("You Must Specify A Teacher");
+
+		$id = $_GET['id'];
+
+		$sql_select_teacher = $conn->prepare("SELECT * FROM TEACHER WHERE TEACHER_ID = ?");
+		$sql_select_teacher->bind_param("i", $id);
+		$sql_select_teacher->execute();
+
+		$teacher = $sql_select_teacher -> get_result() -> fetch_assoc();
+
+		if(!$teacher){
+			die("<p class='alert alert-danger w-75 mx-auto my-4'>This Teacher Doesn't Exist</p>");
+		}
+
+
+		$teacher_id =  $teacher["TEACHER_ID"];
+		$teacher_first_name = $teacher["FIRST_NAME"];
+		$teacher_middle_names = $teacher["MIDDLE_NAMES"];
+		$teacher_last_name = $teacher["LAST_NAME"];
+		$teacher_phone_number = $teacher["PHONE_NUMBER"];
+		
+		echo "
+
+			<form class='pop-up-form' method='POST' action='./edit_teacher.php?id=$id'>
+				<input  class='form-control' name='id' type='hidden'>
+
+				<label for='first_name'>First Name</label>
+				<input id='first_name' placeholder='First Name' type='text' value='$teacher_first_name' class='form-control' name='first_name' />
+
+				<label for='middle_names'>Middle Names</label>
+				<input id='middle_names' placeholder='Middle Names' value='$teacher_middle_names' class='form-control' name='middle_names' />
+
+				<label for='last_name'>Last Name</label>
+				<input id='last_name' placeholder='Last Name' value='$teacher_last_name' class='form-control' name='last_name' />
+
+				<label for='phone_number'>Phone Number</label>
+				<input id='phone_number' placeholder='Phone Number' value='$teacher_phone_number' class='form-control' name='phone_number' />
+
+				<button class='btn btn-warning' type='submit'>Edit</button>
+
+				<a href='./teachers.php'><button type='button' class='btn btn-danger'>Cancel</button></a>
+
+
+			</form>
+
+
+		";
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		$new_teacher_first_name =  $_POST["first_name"];
+		$new_teacher_last_name =  $_POST["last_name"];
+		$new_teacher_middle_names =  $_POST["middle_names"];
+		$new_teacher_phone_number =  $_POST["phone_number"];
+
+		$sql_update_teacher = $conn->prepare('update TEACHER set FIRST_NAME = ?, MIDDLE_NAMES = ?, LAST_NAME = ?, PHONE_NUMBER = ? where teacher_ID = ?');
+		$sql_update_teacher->bind_param("ssssi", $new_teacher_first_name, $new_teacher_middle_names, $new_teacher_last_name, $new_teacher_phone_number, $id);
+		$sql_update_teacher->execute();
+
+		header('Location: teachers.php');
+
+	}
+
+?>
+</body>
+</html>
---
+++ teachers/teachers.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Teachers</title>
+	<?php include('../utilities/utilities.php'); ?>
+</head>
+
+<body>
+	<h1>Teachers</h1>
+	<button class="btn btn-primary mx-2" onClick="addClassTo('shown-pop-up', document.getElementById('addTeacherForm'))">Add Teacher</button>
+	<form  class="pop-up-form hidden-pop-up" id="addTeacherForm" method="POST" action="teachers.php">
+		<input required class="form-control" type="text" name="teacher_first_name" placeholder="First Name">
+		<input class="form-control" type="text" name="teacher_middle_names" placeholder="Middle Names">
+		<input required class="form-control" type="text" name="teacher_last_name" placeholder="Last Name">
+		<input required class="form-control" type="text" name="teacher_phone_number" placeholder="Phone Number">
+		<div class="buttons">
+			<button class="btn btn-success" type="submit">Add</button>
+			<button type="button" class="btn btn-danger" onClick="removeClassFrom('shown-pop-up', document.getElementById('addTeacherForm'))">Cancel</button>
+		</div>
+	</form>
+	<?php
+		if($_SERVER["REQUEST_METHOD"] == "POST"){
+				$teacher_first_name = is_valid($_POST["teacher_first_name"], 20);
+				$teacher_middle_names = is_valid($_POST["teacher_middle_names"], 100);
+				$teacher_last_name = is_valid($_POST["teacher_last_name"], 20);
+				$sanitized_phone_number = filter_var($_POST["teacher_phone_number"], FILTER_SANITIZE_NUMBER_INT);
+				$teacher_phone_number = is_valid($sanitized_phone_number, 25);
+
+				if (!($teacher_first_name && $teacher_middle_names && $teacher_last_name && $teacher_phone_number)) {
+					die("<p class='alert alert-danger w-75 mx-auto my-4'>Your data is too long</p>");
+				} 
+	
+				if($conn->connect_error){
+					die("Failed Connection") . $conn->connect_error;
+				}
+
+				$sql_insert_teacher = $conn->prepare("INSERT INTO TEACHER(FIRST_NAME, MIDDLE_NAMES, LAST_NAME, PHONE_NUMBER) values (?, ?, ?, ?)");
+				if($sql_insert_teacher){
+					$sql_insert_teacher->bind_param(
+						"ssss", 
+						$teacher_first_name,
+						$teacher_middle_names,
+						$teacher_last_name,
+						$teacher_phone_number
+					);
+					$sql_insert_teacher->execute();
+					header("Location: teachers.php?success=1");
+				}
+				 else {
+					die("<p class='alert alert-danger w-75 mx-auto my-4'>Your data is corrupter</p>");
+				}
+			} else {
+				// GET Request 
+				if(isset($_GET["success"])){
+					echo ("<p class='alert alert-success w-75 mx-auto my-4'>The Teacher Was Added Successfully </p>");
+				}
+			}
+		?>
+
+
+		<!--  start Showing the data -->
+		<table>
+			<tr>
+				<th>ID</th>
+				<th>Teacher Name</th>
+				<th>Teacher Number</th>
+				<th>Actions</th>
+			</tr>
+
+			<?php
+				$sql_select_teacher = $conn->prepare("SELECT * FROM TEACHER");
+				$sql_select_teacher->execute();
+				$teacher_data = $sql_select_teacher->get_result();
+				while($teacher = $teacher_data->fetch_assoc()){
+					$teacher_full_name = $teacher["FIRST_NAME"]. " " . $teacher["MIDDLE_NAMES"] . " " . $teacher["LAST_NAME"];
+					echo "<tr>";
+						echo "<td>" . $teacher["TEACHER_ID"] . "</td>";
+						echo "<td>" . $teacher_full_name . "</td>";
+						echo "<td>" . $teacher["PHONE_NUMBER"] . "</td>";
+						echo "<td>
+							<a href='./edit_teacher.php?id=$teacher[TEACHER_ID]'><button class='btn btn-secondary'>Edit</button></a>
+							<a href='./confirm_delete_teacher.php?id=$teacher[TEACHER_ID]'><button class='btn btn-danger'>Delete</button></a>
+						</td>";
+					echo "<tr/>";
+				}
+
+			?>
+		</table>
+</body>
+</html>
